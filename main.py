18e565b720168640d4d49611f7820d587912e45c
Mark Skipper <marks@aptivate.org>
2015-09-22 20:47:13 +0100

    Add signal handler to close gracefully on ^c
    Despit the doom and gloom here:
    http://stackoverflow.com/questions/17101502/how-to-stop-the-tornado-web-server-with-ctrlc
    This appears to work consistently from the comand line in a few tests.
    
    I've switched from using IOLoop.current() to Instance for the stat
    call but this ought to be okay in the context of the main app since
    I'n not likely to try  and call this code from tests or anything which
    will create separate ioloop instances.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import signal
 import tornado.ioloop
+
 from webapp.application import Application
+
+ioloop = tornado.ioloop.IOLoop.instance()
+
+
+def signal_handler(signum, frame):
+    ioloop.add_callback_from_signal(ioloop.stop)
 
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     Application().listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    ioloop.start()
 
 
 if __name__ == "__main__":
